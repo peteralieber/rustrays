1ac9eb096e400dc279db58d36459c6dacf7ba115
Peter Lieber <41489571+peteralieber@users.noreply.github.com>
2021-05-11 20:21:21 -0500

    implemented basic operators

--- src/main.rs
+++ src/main.rs
@@ -5,5 +5,14 @@ mod vectors;
 fn main() {
     //println!("RustRays!");
     
-    util::output_image();
+    //util::output_image();
+
+    let v1 = vectors::Point3 {x:1.0,y:1.0,z:1.0};
+    let v2 = vectors::Point3 {x:1.0,y:2.0,z:3.0};
+    let v3 = vectors::Point3 {x:2.0,y:2.0,z:2.0};
+
+    eprintln!("Vector (1,1,1) + (1,2,3) = {}", v1+v2);
+    eprintln!("Vector (2,2,2) * (1,2,3) = {}", v3*v2);
+    eprintln!("Vector (2,2,2) . (1,2,3) = {}", v3.dot(v2));
+    eprintln!("Vector (2,2,2) x (1,2,3) = {}", v3.cross(v2));
 }
--- src/vectors.rs
+++ src/vectors.rs
@@ -3,13 +3,29 @@ use std::ops::*;
 
 pub trait Length {
     fn length(&self) -> f32;
+    fn length_squared(&self) -> f32;
 }
 
-pub struct Point3(f32, f32, f32);
+#[derive(Copy, Clone)]
+pub struct Point3 {
+    pub x: f32,
+    pub y: f32,
+    pub z: f32
+}
+
+impl std::fmt::Display for Point3 {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "({}, {}, {})", self.x, self.y, self.z)
+    }
+}
 
 impl Length for Point3 {
     fn length(&self) -> f32 {
-        (self.0 * self.0 + self.1 * self.1 + self.2 * self.2).sqrt()
+        (self.x * self.x + self.y * self.y + self.z * self.z).sqrt()
+    }
+
+    fn length_squared(&self) -> f32 {
+        self.x * self.x + self.y * self.y + self.z * self.z
     }
 }
 
@@ -17,10 +33,84 @@ impl Add for Point3 {
     type Output = Self;
 
     fn add(self, other: Self) -> Self {
-        Point3(
-            self.0 + other.0,
-            self.1 + other.1,
-            self.2 + other.2
-        )
+        Point3 {
+            x: self.x + other.x,
+            y: self.y + other.y,
+            z: self.z + other.z
+        }
+    }
+}
+
+impl Sub for Point3 {
+    type Output = Self;
+
+    fn sub(self, other: Self) -> Self {
+        Point3 {
+            x: self.x - other.x,
+            y: self.y - other.y,
+            z: self.z - other.z
+        }
+    }
+}
+
+impl Mul for Point3 {
+    type Output = Self;
+
+    fn mul(self, other: Self) -> Self {
+        Point3 {
+            x: self.x * other.x,
+            y: self.y * other.y,
+            z: self.z * other.z
+        }
+    }
+}
+
+impl Mul<f32> for Point3 {
+    type Output = Self;
+
+    fn mul(self, other: f32) -> Self {
+        Point3 {
+            x: self.x * other,
+            y: self.y * other,
+            z: self.z * other
+        }
+    }
+}
+
+impl Mul<Point3> for f32 {
+    type Output = Point3;
+
+    fn mul(self, other: Point3) -> Point3 {
+        Point3 {
+            x: self * other.x,
+            y: self * other.y,
+            z: self * other.z
+        }
+    }
+}
+
+impl Div for Point3 {
+    type Output = Self;
+
+    fn div(self, other: Self) -> Self {
+        Point3 {
+            x: self.x / other.x,
+            y: self.y / other.y,
+            z: self.z / other.z
+        }
+    }
+}
+
+impl Point3 {
+    pub fn dot(self, other: Self) -> f32 {
+        self.x*other.x + self.y*other.y + self.z*other.z
+    }
+
+    pub fn cross(self, other: Self) -> Self {
+        Point3 {
+            x: self.y * other.z - self.z * other.y,
+            y: self.z * other.x - self.x * other.z,
+            z: self.x * other.y - self.y * other.x
+        }
     }
 }
